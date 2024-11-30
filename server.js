7c17c162202e05667be494822ed5cf9b1c9edf55
waleedahmad36 <waleed09ahmad42@gmail.com>
2024-11-30 02:08:13 -0800

    one more

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
