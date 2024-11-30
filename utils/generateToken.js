21f5111e4e04acb0a650ad5c85ba6d5f91648777
waleedahmad36 <waleed09ahmad42@gmail.com>
2024-11-30 01:56:36 -0800

    something

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -7,7 +7,8 @@ export const generateTokenAndSetCookie = (userId, res) => {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "lax",  // Changed from strict to lax
-    secure: process.env.NODE_ENV !== "development" ? true : false,
+    // secure: process.env.NODE_ENV !== "development" ? true : false,
+    secure : false
   });
 
   return token;
