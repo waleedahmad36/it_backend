c34d47c236733e9476ef937a93692f5d00cb2ef3
waleedahmad36 <waleed09ahmad42@gmail.com>
2024-11-30 00:09:00 -0800

    issues

--- db/db.js
+++ db/db.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI);
-		console.log("MongoDB connected: " + conn.connection.host);
-	} catch (error) {
-		console.error("Error connecting to MONGODB: " + error.message);
-		process.exit(1); // 1 means there was an error, 0 means success
-	}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      retryWrites: true,  // Explicitly setting retryWrites to true
+    });
+    console.log("MongoDB connected: " + conn.connection.host);
+  } catch (error) {
+    console.error("Error connecting to MONGODB: " + error.message);
+    process.exit(1); // Exit process with failure code
+  }
 };
