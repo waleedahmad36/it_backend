15da8b01bf4fc3e651a83c2fb9f4bde5263a2e88
waleedahmad36 <waleed09ahmad42@gmail.com>
2024-11-29 23:52:49 -0800

    first commit

---
+++ routes/userAuthRoutes.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import { login, logout, signup,authCheck , updateProfile , fetchAllInstructors} from '../controllers/userController.js';
+import {protectRoute} from '../middleware/protectRoute.js'
+import upload from '../utils/multer.js';
+const router = express.Router();
+
+
+router.post('/signup',signup)
+
+
+
+router.post('/login',login)
+
+router.post('/logout',logout)
+
+
+router.get('/authCheck',protectRoute,authCheck)
+router.put("/profile/update", protectRoute, upload.single("profilePic"), updateProfile);
+router.get('/instructors',fetchAllInstructors)
+
+
+
+export default router;
---
+++ server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/db.js";
+import cookieParser from "cookie-parser";
+import userAuthRoutes from "./routes/userAuthRoutes.js";
+import courseRoutes from "./routes/courseRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import quizRoutes from "./routes/quizRoutes.js";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "50mb" }));
+app.use(cookieParser());
+
+// API Routes
+app.use("/api/v1/auth", userAuthRoutes);
+app.use("/api/v1/course", courseRoutes);
+app.use("/api/v1/admin", adminRoutes);
+app.use("/api/v1/quiz", quizRoutes);
+
+// Start Server
+app.listen(process.env.PORT || 5000, async () => {
+  console.log(`Server is running on port ${process.env.PORT || 5000}`);
+  await connectDB();
+});
